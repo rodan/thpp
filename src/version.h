e0b488c1f940280699311ece18266e066cd5dbb5
Petre Rodan <2b4eda@subdimension.ro>
2023-04-02 17:29:39 +0300

    cleanup

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Sun Apr  2 12:37:10 UTC 2023
+// compiled on Sun Apr  2 12:43:52 UTC 2023
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 2562
-#define COMMIT 83
+#define BUILD 2564
+#define COMMIT 84
 
 #endif
