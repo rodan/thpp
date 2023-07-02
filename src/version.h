c67de66893b428aefbcdebbf8bf485ddc895d191
Petre Rodan <2b4eda@subdimension.ro>
2023-07-02 18:29:12 +0300

    TODO refocusing

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Tue Jun 20 16:33:47 UTC 2023
+// compiled on Sun Jul  2 08:14:10 UTC 2023
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 3050
-#define COMMIT 109
+#define BUILD 3051
+#define COMMIT 110
 
 #endif
