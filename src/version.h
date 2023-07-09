a253a0c2a1d55259a37988091f2cec5e506a8454
Petre Rodan <2b4eda@subdimension.ro>
2023-07-09 06:50:51 +0300

    remove imgui.ini from repo

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Fri Jul  7 14:43:15 UTC 2023
+// compiled on Fri Jul  7 14:57:02 UTC 2023
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 3163
-#define COMMIT 114
+#define BUILD 3165
+#define COMMIT 115
 
 #endif
