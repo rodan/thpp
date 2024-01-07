10fabc70e0d6daed0f8e46f92aaceec371fe74ca
Petre Rodan <petre.rodan@subdimension.ro>
2024-01-07 14:13:27 +0200

    fix Makefile target order, do not use CFLAGS/LDFLAGS to store mandatory flags

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Wed Jul 19 10:57:36 UTC 2023
+// compiled on Wed Dec 20 16:21:05 UTC 2023
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 3222
-#define COMMIT 125
+#define BUILD 3223
+#define COMMIT 129
 
 #endif
